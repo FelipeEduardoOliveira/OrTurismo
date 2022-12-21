24b712bb89689a4a38edd2544a2687ca5402d47c
Felipe Oliveira <felipe.unifieo@gmail.com>
2022-12-21 19:19:36 -0300

    git push build

---
+++ build/precache-manifest.5c55d49f3021a6aa0efbdb22d5dfd62e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "720f136feb5001adfd2d",
+    "url": "/static/css/main.5370557d.chunk.css"
+  },
+  {
+    "revision": "720f136feb5001adfd2d",
+    "url": "/static/js/main.720f136f.chunk.js"
+  },
+  {
+    "revision": "9240f9f763df1a6e54e0",
+    "url": "/static/js/1.9240f9f7.chunk.js"
+  },
+  {
+    "revision": "2168e1ceb77c5474a67c",
+    "url": "/static/js/2.2168e1ce.chunk.js"
+  },
+  {
+    "revision": "fb0b4c6b38ee98968d6f",
+    "url": "/static/js/runtime~main.fb0b4c6b.js"
+  },
+  {
+    "revision": "4a370759c75681237762d3638af0605c",
+    "url": "/static/media/Logo.4a370759.png"
+  },
+  {
+    "revision": "f694176b70a1304e65de94d258b585ac",
+    "url": "/static/media/CapaBanner.f694176b.png"
+  },
+  {
+    "revision": "990bd36e12154c548f4892327ca7ebdd",
+    "url": "/static/media/bater-papo.990bd36e.png"
+  },
+  {
+    "revision": "8fe613ee05c548ad08f51bd208ce6384",
+    "url": "/static/media/portfolio.8fe613ee.png"
+  },
+  {
+    "revision": "58960a9bcd630cf93e7e3aa6eca98d35",
+    "url": "/static/media/apresentacao.58960a9b.png"
+  },
+  {
+    "revision": "3dd2dfcef0a2b150bdcf20df4cedaaed",
+    "url": "/static/media/viagem.3dd2dfce.png"
+  },
+  {
+    "revision": "e08a2dc6ae75e5d09169444179e67d53",
+    "url": "/static/media/backpacker.e08a2dc6.jpg"
+  },
+  {
+    "revision": "51f1380dd2146d90b5aa89fd9360893b",
+    "url": "/static/media/happy-tourist.51f1380d.jpg"
+  },
+  {
+    "revision": "8b357b47f00b0b898a8ba58203557582",
+    "url": "/static/media/whatsapp.8b357b47.png"
+  },
+  {
+    "revision": "4267d8aa8711bb8c72cbefb26066c9e0",
+    "url": "/static/media/bookman-old-style.4267d8aa.ttf"
+  },
+  {
+    "revision": "881e150ab929e26d1f812c4342c15a7c",
+    "url": "/static/media/Roboto-Light.881e150a.ttf"
+  },
+  {
+    "revision": "1853a578246d1b2182b73ee5fadd57b7",
+    "url": "/static/media/NotoSans-Light.1853a578.ttf"
+  },
+  {
+    "revision": "474f7dd11c54b58f9b78ab72d134aaa3",
+    "url": "/static/media/amigas--paris.474f7dd1.jpg"
+  },
+  {
+    "revision": "cfdbb0c5a0e36f68e4a073695c666946",
+    "url": "/index.html"
+  }
+];
